755f78b2af8edeb61161aed7232d11eebe8dbd24
Gil Campos <gilberto.campos@livinapp.net>
2024-10-09 11:37:35 -0600

    Finish doing lecture with LinearLayout

--- Week-1/Day-2/LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ Week-1/Day-2/LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userName: TextView
+    private lateinit var userInput: EditText
+    private lateinit var finishApp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        getUserData()
+    }
+
+
+    private fun initComponents() {
+        userName = findViewById(R.id.tvUserName)
+        userInput = findViewById(R.id.etUserInput)
+        finishApp = findViewById(R.id.btnFinish)
+    }
+
+    private fun getUserData() {
+        finishApp.setOnClickListener {
+            val data = userInput.text.toString()
+            userName.text = data
+        }
+
     }
 }
